33022660a1fb6b1a88c6017881893c743c189494
Tony Yu <tonynjupt@gmail.com>
2025-10-29 14:06:51 +0800

    Update stdc1_10k_crack-544x384.py

--- configs/stdc/stdc1_10k_crack-544x384.py
+++ configs/stdc/stdc1_10k_crack-544x384.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/stdc.py', '../_base_/datasets/data.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_8k.py'
 ]
 crop_size = (544, 384)
 data_preprocessor = dict(size=crop_size)
@@ -11,13 +11,13 @@ model = dict(
     )
 )
 param_scheduler = [
-    dict(type='LinearLR', by_epoch=False, start_factor=0.1, begin=0, end=20000),
+    dict(type='LinearLR', by_epoch=False, start_factor=0.1, begin=0, end=8000),
     dict(
         type='PolyLR',
         eta_min=1e-4,
         power=0.9,
         begin=10,
-        end=20000,
+        end=8000,
         by_epoch=False,
     )
 ]
@@ -25,4 +25,5 @@ param_scheduler = [
 vis_backends = [dict(type='LocalVisBackend'),
                 dict(type='TensorboardVisBackend')]
 visualizer = dict(
+
     type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer',save_dir='work_dirs/vis_results')
