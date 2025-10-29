1bee0c8cf3b76adad7631b41ad58d840f39590ac
Tony Yu <tonynjupt@gmail.com>
2025-10-29 13:26:44 +0800

    Add files via upload

---
+++ configs/stdc/stdc1_10k_crack-544x384.py
@@ -0,0 +1,28 @@
+_base_ = [
+    '../_base_/models/stdc.py', '../_base_/datasets/data.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
+]
+crop_size = (544, 384)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=dict(
+        num_classes=2,
+    )
+)
+param_scheduler = [
+    dict(type='LinearLR', by_epoch=False, start_factor=0.1, begin=0, end=20000),
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=10,
+        end=20000,
+        by_epoch=False,
+    )
+]
+
+vis_backends = [dict(type='LocalVisBackend'),
+                dict(type='TensorboardVisBackend')]
+visualizer = dict(
+    type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer',save_dir='work_dirs/vis_results')
